6ad342e8a5da44f46800521633a1d6f3c43b255e
“antoanparashkevov” <tonkata1505@gmail.com>
2024-02-10 15:40:38 +0200

    Modify the linear gradient from white to purple

--- src/components/UI/LinearGradient.tsx
+++ src/components/UI/LinearGradient.tsx
@@ -5,7 +5,7 @@ const LinearGradient: React.FC<{ className?: string }> = ({ className }) => {
 		<div
 			className={`absolute inset-0 -z-10 pointer-events-none ${className}`}
 			style={{
-				background: 'linear-gradient(0deg,rgba(255, 206, 231, 0.8) 0%, rgba(255, 228, 242, 0.8) 35%, rgba(255, 168, 213, 0.3) 100%)',
+				background: 'linear-gradient(0deg,rgba(255, 206, 231, 0.8) 0%, rgba(255, 220, 238, 0.8) 35%, rgba(255, 255, 255, 0.5) 100%)',
 				filter: "blur(120px)"
 			}}
 		/>
